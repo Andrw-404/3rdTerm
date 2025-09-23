231a1473607c8e0c1b480c18d0c54fd0a8e3e881
Andrew Kalinin <Fipett2407@gmail.com>
2025-09-24 00:26:20 +0300

    add: a single-threaded solution tests

---
+++ LazyInterface/LazyInterface.Tests/SingleThreadedTests.cs
@@ -0,0 +1,46 @@
+﻿// <copyright file="SingleThreadedTests.cs" company="Kalinin Andrew">
+// Copyright (c) Kalinin Andrew. All rights reserved.
+// </copyright>
+
+namespace LazyInterface.Tests
+{
+    public class SingleThreadedTests
+    {
+        [Test]
+        public void Get_ShoulCallSupplierOnlyOnceAndReturnCorrectValue_MultipleCalls()
+        {
+            int callCount = 0;
+            Func<string> supplier = () =>
+            {
+                callCount++;
+                return "abc";
+            };
+            var lazy = new SimpleVersion<string>(supplier);
+
+            lazy.Get();
+            lazy.Get();
+            lazy.Get();
+
+            var result = lazy.Get();
+
+            Assert.That(callCount, Is.EqualTo(1));
+            Assert.That(result, Is.EqualTo("abc"));
+        }
+
+        [Test]
+        public void Get_ShoulReturnTheSameObjectReference_MultipleCalls()
+        {
+            var lazy = new SimpleVersion<List<string>>(() => []);
+
+            var result1 = lazy.Get();
+            var result2 = lazy.Get();
+            Assert.That(result1, Is.SameAs(result2));
+        }
+
+        [Test]
+        public void Constructor_ShouldThrowArgumentNullException_SupplierIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new SimpleVersion<object>(null!));
+        }
+    }
+}
