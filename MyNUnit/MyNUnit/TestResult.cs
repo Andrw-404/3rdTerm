40a7b4ad713186cdf1c920c74e2494c0b5be2bfa
Andrew Kalinin <Fipett2407@gmail.com>
2025-11-24 02:35:37 +0300

    add: started completing the task

---
+++ MyNUnit/MyNUnit/TestResult.cs
@@ -0,0 +1,12 @@
+﻿namespace MyNUnit;
+
+public class TestResult
+{
+    public string ClassName {  get; set; }
+    public string MethodName {  get; set; }
+    public bool IsSuccess {  get; set; }
+    public bool IsIgnored {  get; set; }
+    public string IgnoreReason {  get; set; }
+    public string ErrorMessage {  get; set; }
+    public TimeSpan TestTime {  get; set; }
+}
---
+++ MyNUnit/MyNUnit/TestRunner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNUnit
+{
+    public class TestRunner
+    {
+        public void RunTest(string path)
+        {
+            var dlls = Directory.GetFiles(path, "*.dll", SearchOption.AllDirectories);
+            var results = new ConcurrentBag<TestResult>();
+
+            Parallel.ForEach(dlls, (dllPath) =>
+            {
+                try
+                {
+                    var assembly = Assembly.LoadFrom(dllPath);
+                    var testClasses = assembly.GetTypes();
+                }
+            }
+
+        }
+
+    }
+}
