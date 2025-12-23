0dbaf4ed6a1d2120e6bcae33b26b48aa41291cad
Andrew Kalinin <Fipett2407@gmail.com>
2025-12-23 15:19:38 +0300

    add: task

---
+++ thirdTestTask/thirdTestTask/ServerHost.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thirdTestTask;
+
+public static class ServerHost
+{
+    public static async Task RunOnce(int port)
+    {
+        var listner = new TcpListener(IPAddress.Any, port);
+        listner.Start();
+
+        try
+        {
+            Console.WriteLine($"[Сервер] Ожидание подлюкчения на порту {port}...");
+
+            using TcpClient client = await listner.AcceptTcpClientAsync();
+
+            Console.WriteLine("[Сервер] Клиент подключился. Для выхода - exit");
+            await ChatSession.RunAsync(client, Console.In, Console.Out);
+        }
+
+        finally
+        {
+            listner.Stop();
+            Console.WriteLine("[Сервер] Завершение");
+        }
+    }
+}
