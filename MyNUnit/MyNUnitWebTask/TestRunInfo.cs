43c056f74bd8df71ad979fad5e5c5465e3392e2c
Andrew Kalinin <Fipett2407@gmail.com>
2025-12-27 01:51:12 +0300

    add: TestRunInfo and TestRunService classes

---
+++ MyNUnit/MyNUnitWebTask/TestRunInfo.cs
@@ -0,0 +1,21 @@
+﻿namespace MyNUnitWebTask;
+
+public class TestInfo
+{
+    public string TestName { get; set; } = string.Empty;
+    public string Status {  get; set; } = string.Empty;
+    public long ExecutionTime { get; set; }
+    public string Message { get; set; } = string.Empty; 
+}
+
+public class TestRunInfo
+{
+    public int RunId {  get; set; }
+    public DateTime RunTime {  get; set; }
+    public List<string> LoadedAssemblies {  get; set; } = new List<string>();
+    public List<TestInfo> Tests { get; set; } = new List<TestInfo>();
+    public int PassedCount => Tests.Count(x => x.Status == "Passed");
+    public int FailedCount => Tests.Count(x => x.Status == "Failed");
+    public int IgnoredCount => Tests.Count(x => x.Status == "Ignored");
+
+}
---
+++ MyNUnit/MyNUnitWebTask/TestRunService.cs
@@ -0,0 +1,65 @@
+﻿using MyNUnit;
+
+namespace MyNUnitWebTask;
+
+public class TestRunService
+{
+    private static List<TestRunInfo> allRuns = new List<TestRunInfo>();
+    private static int nextRunId = 1;
+
+    public TestRunInfo RunTests(List<string> assemblyPaths)
+    {
+        var testRuner = new TestRunner();
+
+        var tmpDir = Path.Combine(Path.GetTempPath(), $"MyNUnit_{Guid.NewGuid()}");
+        Directory.CreateDirectory(tmpDir);
+
+        try
+        {
+            foreach (var assemblyPath in assemblyPaths)
+            {
+                if (File.Exists(assemblyPath))
+                {
+                    var fileName = Path.GetFileName(assemblyPath);
+                    File.Copy(assemblyPath, Path.Combine(tmpDir, fileName), true);
+                }
+            }
+
+            var testResult = testRuner.RunTest(tmpDir);
+            var runInfo = new TestRunInfo
+            {
+                RunId = nextRunId++,
+                RunTime = DateTime.Now,
+                LoadedAssemblies = assemblyPaths.Select(Path.GetFileName).ToList() ?? new(),
+                Tests = testResult.Select(x => new TestInfo
+                {
+                    TestName = $"{x.ClassName}.{x.MethodName}",
+                    Status = x.IsIgnored ? "Ignored" : (x.IsSuccess ? "Passed" : "Failed"),
+                    ExecutionTime = (long)x.TestTime.TotalMilliseconds,
+                    Message = x.IsIgnored ? x.IgnoreReason ?? "" : x.ErrorMessage ?? ""
+                }).ToList()
+            };
+
+            allRuns.Add(runInfo);
+            return runInfo;
+        }
+        finally
+        {
+            try
+            {
+                Directory.Delete(tmpDir, true);
+            }
+            catch { }
+        }
+    }
+
+    public List<TestRunInfo> GetHistory()
+    {
+        return allRuns.OrderByDescending(x => x.RunId).ToList();
+    }
+
+    public TestRunInfo? GetLastRun()
+    {
+        return allRuns.OrderByDescending(x => x.RunId).FirstOrDefault();
+    }
+}
