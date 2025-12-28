03ad62bb52bd6e086b55213c6c3286aa8adad8f8
Andrew Kalinin <Fipett2407@gmail.com>
2025-12-29 01:37:47 +0300

    test: add more tests

--- MyThreadPoolTask/MyThreadPoolTask.Tests/ThreadPoolTests.cs
+++ MyThreadPoolTask/MyThreadPoolTask.Tests/ThreadPoolTests.cs
@@ -101,4 +101,105 @@ public class ThreadPoolTests
 
         startSignal.Set();
     }
+
+    [Test]
+    public void ContinueWith_MultipleContinuations_AllShouldExecute()
+    {
+        this.threadPool = new MyThreadPool(4);
+        var baseTask = this.threadPool.Submit(() => 10);
+
+        var continuation1 = baseTask.ContinueWith(x => x + 1);
+        var continuation2 = baseTask.ContinueWith(x => x + 2);
+        var continuation3 = baseTask.ContinueWith(x => x * 2);
+
+        Assert.That(continuation1.Result, Is.EqualTo(11));
+        Assert.That(continuation2.Result, Is.EqualTo(12));
+        Assert.That(continuation3.Result, Is.EqualTo(20));
+    }
+
+    [Test]
+    public void ContinueWith_OnCompletedTask_ShouldStillWork()
+    {
+        this.threadPool = new MyThreadPool(2);
+        var task = this.threadPool.Submit(() => 5);
+        var result = task.Result;
+
+        Assert.That(task.IsCompleted, Is.True);
+
+        var continuation = task.ContinueWith(x => x * 10);
+        Assert.That(continuation.Result, Is.EqualTo(50));
+    }
+
+    [Test]
+    public void Result_BeforeCompletion_ShouldBlockUntilComplete()
+    {
+        this.threadPool = new MyThreadPool(2);
+        var startSignal = new ManualResetEventSlim(false);
+        var task = this.threadPool.Submit(() =>
+        {
+            startSignal.Wait();
+            return 4;
+        });
+
+        Assert.That(task.IsCompleted, Is.False);
+
+        var resultTask = Task.Run(() => task.Result);
+
+        Thread.Sleep(100);
+        Assert.That(resultTask.IsCompleted, Is.False);
+
+        startSignal.Set();
+        Assert.That(resultTask.Result, Is.EqualTo(4));
+        Assert.That(task.IsCompleted, Is.True);
+    }
+
+    [Test]
+    public void ContinueWith_AfterShutdown_ShouldThrowException()
+    {
+        this.threadPool = new MyThreadPool(2);
+        var task = this.threadPool.Submit(() => 10);
+        var result = task.Result;
+
+        this.threadPool.Shutdown();
+
+        Assert.Throws<InvalidOperationException>(() => task.ContinueWith(x => x * 2));
+        this.threadPool = null;
+    }
+
+    [TestCase(0)]
+    [TestCase(-1)]
+    public void Constructor_InvalidThreadCount_ShouldThrowException(int count)
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new MyThreadPool(count));
+    }
+
+    [Test]
+    public void ContinueWith_WhenShutdownDuringExecution_ShouldThrowExceptionInResult()
+    {
+        this.threadPool = new MyThreadPool(1);
+        var blockSignal = new ManualResetEventSlim(false);
+        var taskStarted = new ManualResetEventSlim(false);
+
+        var baseTask = this.threadPool.Submit(() =>
+        {
+            taskStarted.Set();
+            blockSignal.Wait();
+            return 4;
+        });
+
+        taskStarted.Wait();
+
+        var continuation = baseTask.ContinueWith(x => x * 2);
+        var shutdownThread = new Thread(() => this.threadPool!.Shutdown());
+        shutdownThread.Start();
+
+        Thread.Sleep(100);
+        blockSignal.Set();
+
+        shutdownThread.Join(TimeSpan.FromSeconds(2));
+        var exception = Assert.Throws<AggregateException>(() => _ = continuation.Result);
+        Assert.That(exception!.InnerException, Is.InstanceOf<InvalidOperationException>());
+
+        this.threadPool = null;
+    }
 }
